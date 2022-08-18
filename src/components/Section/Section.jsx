6ad2ecdde0fce0e7666b5268e7983014abbc8845
SerhiyDukh1984 <s.dux2011@ukr.net>
2022-08-18 21:41:33 +0300

    that is all

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,11 +1,5 @@
 import s from './Section.module.css';
 
 export default function Section({ children }) {
-  return (
-    <section className={s.section}>
-      {/* <img src="../../icons/tryzub5.svg" alt=""></img> */}
-      {/* {title && <h1 className={s.title}>{title}</h1>} */}
-      {children}
-    </section>
-  );
+  return <section className={s.section}>{children}</section>;
 }
